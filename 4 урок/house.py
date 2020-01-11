3d76a41ef4fd6003f9070383b76aefc975c3b1c2
Burimov Stepan <sonyliwe@mail.ru>
2020-01-11 18:52:04 +0300

    Конец рисования домика.

--- 4 урок/house.py
+++ 4 урок/house.py
@@ -5,11 +5,16 @@ def main():
     paint_house(200, 200, 300, 400)
     run()
 
+
 def paint_house(x, y, width, height):
     """ Рисует домик
         (x, y) -- опорная точка, левая-нижняя точка крыши
     """
-    pass
+    paint_walls(x, y, width, height // 2)
+    paint_roof(x, y, width, height // 2)
+    w_width = width // 3
+    w_height = height //6
+    paint_window(x + w_width, y + w_height, w_width, w_height)
 
 
 main()
