630c643e92ea55f40cb38359e399ce22b7704d1b
Burimov Stepan <sonyliwe@mail.ru>
2020-01-12 14:56:59 +0300

    Первая программа с использованием Tkinter

--- 5 урок/hello_world.py
+++ 5 урок/hello_world.py
@@ -1,8 +1,23 @@
 from tkinter import *
 
+def handler1(event):
+    print('Hello World! x=', event.x, 'y=', event.y)
+
+def handler2(event):
+    exit()
+
+# инициализация
 root = Tk()
+hello_label = Label(root, text="Hello, world!", font="Times 40")  
+hello_label.pack()
 
-w = Label(root, text="Hello,  World!")
-w.pack()
+# Привязка обработчиков - к событию и виджету:
+# виджет.bind(событие, обработчик)
+hello_label.bind('<Button-1>', handler1)
+hello_label.bind('<Button-3>', handler2)
 
+
+# главный цикл (проект)
 root.mainloop()
+
+print('Программа заканчивается. Вышли из главного цикла.')
