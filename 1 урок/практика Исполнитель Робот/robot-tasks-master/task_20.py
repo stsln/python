83c473301b54477a133c484edb0541f82ceb96eb
Stepan <34548685+stsln@users.noreply.github.com>
2020-02-13 20:24:06 +0300

    Сделал 20 и 21 задание

--- 1 урок/практика Исполнитель Робот/robot-tasks-master/task_20.py
+++ 1 урок/практика Исполнитель Робот/robot-tasks-master/task_20.py
@@ -5,6 +5,23 @@ from pyrob.api import *
 
 @task(delay=0.05)
 def task_4_3():
+    b = 0
+    while b < 6:
+        a = 54
+        b += 1
+        move_right()
+        while a > 27:
+            fill_cell()
+            move_right()
+            a -= 1
+        move_down()
+        move_left()
+        while a > 0:
+            fill_cell()
+            move_left()
+            a -= 1
+        move_down()
+    move_right()
     pass
 
 
--- 1 урок/практика Исполнитель Робот/robot-tasks-master/task_21.py
+++ 1 урок/практика Исполнитель Робот/robot-tasks-master/task_21.py
@@ -5,6 +5,22 @@ from pyrob.api import *
 
 @task(delay=0.05)
 def task_4_11():
+    move_down()
+    a = 1
+    for i in range(13):
+        
+        while not cell_is_filled():
+            move_right(a)
+            if not wall_is_on_the_right():
+                fill_cell()
+            a += 1
+        
+        move_left()
+        while not wall_is_on_the_left():
+            fill_cell()
+            move_left()
+        move_down()
+    move_right()
     pass
 
 
