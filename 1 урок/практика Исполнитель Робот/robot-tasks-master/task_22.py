c247098f7a26fea8f950f0646c8c0591c9facd54
Stepan <34548685+stsln@users.noreply.github.com>
2020-02-25 22:11:54 +0300

    Решил по 23 задачу

--- 1 урок/практика Исполнитель Робот/robot-tasks-master/task_22.py
+++ 1 урок/практика Исполнитель Робот/robot-tasks-master/task_22.py
@@ -5,6 +5,22 @@ from pyrob.api import *
 
 @task
 def task_5_10():
+    a = True
+    while a == True:
+        while not wall_is_on_the_right():
+            fill_cell()
+            move_right()
+        fill_cell()
+
+        if wall_is_on_the_right() and wall_is_beneath():
+            a = False
+            while not wall_is_on_the_left():
+                move_left()
+        else:
+            a = True
+            while not wall_is_on_the_left():
+                move_left()
+            move_down()
     pass
 
 
--- 1 урок/практика Исполнитель Робот/robot-tasks-master/task_23.py
+++ 1 урок/практика Исполнитель Робот/robot-tasks-master/task_23.py
@@ -5,6 +5,26 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_6_6():
+    move_right()
+    b = 0
+    def a():
+        if not wall_is_above():
+            a = 0
+            while not wall_is_above():
+                move_up(n=1)
+                fill_cell()
+                a += 1
+            move_down(a)
+    while not wall_is_on_the_right():
+        if not wall_is_above():
+            a()
+            move_right()
+        else:
+           move_right()
+        b += 1
+    else:
+        a()
+    move_left(b + 1)
     pass
 
 
