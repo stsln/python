be97b43e88133a05fa9d708953356c9064ec6cba
Stepan <34548685+stsln@users.noreply.github.com>
2020-02-26 22:14:06 +0300

    Решил до 26 задачи

--- 1 урок/практика Исполнитель Робот/robot-tasks-master/task_24.py
+++ 1 урок/практика Исполнитель Робот/robot-tasks-master/task_24.py
@@ -5,8 +5,36 @@ from pyrob.api import *
 
 @task
 def task_2_1():
+    def fill():
+        a = 0
+        while a != 3:
+            move_right()
+            fill_cell()
+            a += 1
+
+    move_down(2)
+    fill()
+    move_left()
+    move_down()
+    fill_cell()
+    move_up(2)
+    fill_cell()
+    move_left()
     pass
 
 
 if __name__ == '__main__':
     run_tasks()
+"""
+move_left(n=1)	Пройти n клеток влево (по умолчанию n = 1)
+move_right(n=1)	Пройти n клеток вправо (по умолчанию n = 1)
+move_up(n=1)	Пройти n клеток вверх (по умолчанию n = 1)
+move_down(n=1)	Пройти n клеток вниз (по умолчанию n = 1)
+wall_is_above()	если сверху стена, возвращает True, иначе — False
+wall_is_beneath()	если снизу стена, возвращает True, иначе — False
+wall_is_on_the_left()	если слева стена, возвращает True, иначе — False
+wall_is_on_the_right()	если справа стена, возвращает True, иначе — False
+fill_cell()	Закрасить текущую клетку
+cell_is_filled()	Возвращает True, если текущая клетка закрашена
+mov(r, v)	Поместить значение v в регистр r
+"""
--- 1 урок/практика Исполнитель Робот/robot-tasks-master/task_25.py
+++ 1 урок/практика Исполнитель Робот/robot-tasks-master/task_25.py
@@ -5,6 +5,29 @@ from pyrob.api import *
 
 @task
 def task_2_2():
+    move_down()
+    n = 0
+    while n < 5:
+        n += 1
+        def fill():
+            a = 0
+            while a != 2:
+                fill_cell()
+                a += 1
+                move_right()
+                fill_cell()
+        move_down()
+        fill()
+        move_left()
+        move_down()
+        fill_cell()
+        move_up(2)
+        fill_cell()
+        if n == 5:
+            move_left()
+        else:
+            move_right(3)
+    
     pass
 
 
