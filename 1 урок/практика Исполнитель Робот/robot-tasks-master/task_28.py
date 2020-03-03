178de26d08a715d760c0d8c7f240a543fc19cd1a
Stepan <34548685+stsln@users.noreply.github.com>
2020-03-03 08:35:14 +0300

    Решил по 28 задачу

--- 1 урок/практика Исполнитель Робот/robot-tasks-master/task_28.py
+++ 1 урок/практика Исполнитель Робот/robot-tasks-master/task_28.py
@@ -5,6 +5,15 @@ from pyrob.api import *
 
 @task
 def task_7_6():
+    a = 0
+    b = True
+    while b == True:
+        if a == 5:
+            b = False
+        else:
+            move_right()
+        if cell_is_filled():
+            a += 1
     pass
 
 
