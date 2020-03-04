5d8c878586186662b1d65caebea88a00a45d0b36
Stepan <34548685+stsln@users.noreply.github.com>
2020-03-04 20:50:20 +0300

    Восьмая лекция

--- 8 урок/8u.py
+++ 8 урок/8u.py
@@ -1,2 +1,12 @@
 SOLID-принципы
-Принцип единственной ответственности класса
+Принцип единственной ответственности класса.
+
+KISS-принцип
+keep
+it
+simple
+stupid
+
+FizzBuzz JavaScript
+https://gist.github.com/jaysonrowe/1592432
+https://github.com/EnterpriseQualityCoding/FizzBuzzEnterpriseEdition
