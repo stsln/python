041e03fd18178e54b799f5beaceb0d61a8e66aa2
Stepan <34548685+stsln@users.noreply.github.com>
2020-03-04 21:38:57 +0300

    Восьмая лекция

--- 8 урок/8u.py
+++ 8 урок/8u.py
@@ -10,3 +10,19 @@ stupid
 FizzBuzz JavaScript
 https://gist.github.com/jaysonrowe/1592432
 https://github.com/EnterpriseQualityCoding/FizzBuzzEnterpriseEdition
+
+Лучше обдумывать, что должно быть в классе, на листочке.
+
+Пример:
+
+class Ball
+
+свойства: x, y, dx, dy, R, color
+
+методы:
+__init__(...)
+move()
+show()
+check_inside(x, y)
+check_collisoin(ball)
+
