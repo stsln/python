1867f6207d814b6a09eaa67d915cf2933ad27116
Stepan <34548685+stsln@users.noreply.github.com>
2020-03-05 07:01:59 +0300

    Решил по 29 задачу

--- 1 урок/практика Исполнитель Робот/robot-tasks-master/task_29.py
+++ 1 урок/практика Исполнитель Робот/robot-tasks-master/task_29.py
@@ -5,6 +5,17 @@ from pyrob.api import *
 
 @task
 def task_7_7():
+    a = 0
+    b = True
+    while b == True:
+        if a == 3 or wall_is_on_the_right():
+            b = False
+        else:
+            move_right()
+        if cell_is_filled():
+            a += 1
+        else:
+            a = 0
     pass
 
 
