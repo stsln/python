6a09ff9d8e611eecdefecc6ce96c582045482ac3
Stepan <34548685+stsln@users.noreply.github.com>
2020-03-12 18:00:01 +0300

    Решены все задачи, кроме 30.

--- 1 урок/практика Исполнитель Робот/robot-tasks-master/task_32.py
+++ 1 урок/практика Исполнитель Робот/robot-tasks-master/task_32.py
@@ -5,6 +5,30 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_8_18():
+    count_filled_cells = 0
+    while wall_is_beneath():
+
+        #красим клетки которые между вверхней и нижней стенок
+        if wall_is_above() and wall_is_beneath():
+            fill_cell()    
+        move_right()
+
+        #добавление в ax
+        if not wall_is_beneath() and not wall_is_above():
+            mov("ax", count_filled_cells)
+            return
+        
+        #проверка верхней стены
+        if not wall_is_above():
+            #поднимаемеся вверх
+            while not wall_is_above():
+                move_up(n=1)
+                if cell_is_filled():
+                    count_filled_cells += 1
+                fill_cell()
+            #опускаемся вниз
+            while not wall_is_beneath():
+                move_down(n=1)
     pass
 
 
